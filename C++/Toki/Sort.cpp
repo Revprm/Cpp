19709f08d821b74551a4f985a1f07985255ce8f5
Revprm <Revy.pramana@gmail.com>
2022-12-31 11:27:38 +0700

    added an alternative solution for a certain problem

--- C++/Toki/Sort.cpp
+++ C++/Toki/Sort.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-int main(){
+/*int main(){
     ll N; scanf("%lld", &N);
     ll arr[N];
     for (int i = 0; i < N; i++){
@@ -10,6 +10,22 @@ int main(){
     sort(arr, arr + N);
     for (int i = 0; i < N; i++){
         printf("%d\n", arr[i]);
+    }
+    return 0;
+}
+*/
+int main(){
+    int N, V; scanf("%d", &N);
+    short arr[1001] = {0};
+    for (int i = 0; i < N; i++){
+        scanf("%d", &V);
+        arr[V]++;
+    }
+    for (int i = 0; i < 1001; i++){
+        while (arr[i] > 0){
+            printf("%d\n", i);
+            arr[i]--;
+        }   
     }
     return 0;
 }
