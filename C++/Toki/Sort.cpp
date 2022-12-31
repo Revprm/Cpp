dcbbf6e87aa06e8115d85e6ea03fd2a9a381e98f
Revprm <Revy.pramana@gmail.com>
2022-12-31 11:19:09 +0700

    modified

--- C++/Toki/Sort.cpp
+++ C++/Toki/Sort.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+#define ll long long
 int main(){
-    int N; cin >> N;
-    int arr[N];
+    ll N; scanf("%lld", &N);
+    ll arr[N];
     for (int i = 0; i < N; i++){
-        cin >> arr[i];
+        scanf("%lld", &arr[i]);
     }
     sort(arr, arr + N);
     for (int i = 0; i < N; i++){
-        cout << arr[i] << '\n';
+        printf("%d\n", arr[i]);
     }
     return 0;
 }
