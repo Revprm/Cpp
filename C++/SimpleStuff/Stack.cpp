55e697a5437905202757cbf42ee524199368401a
Revprm <Revy.pramana@gmail.com>
2023-01-06 13:02:21 +0700

    Notes

---
+++ C++/SimpleStuff/Stack.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    stack <int> stck;
+    int N; cin >> N;
+    while (N--){
+        int num; cin >> num;
+        stck.push(num);
+
+    }
+    while (!stck.empty()){
+        cout << stck.top() << " ";
+        stck.pop();
+    }
+    return 0;
+}
