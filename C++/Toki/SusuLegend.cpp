174e83c790fcfe58ad36d52f344600f423d01052
Revprm <Revy.pramana@gmail.com>
2023-01-08 07:40:57 +0700

    first commit

---
+++ C++/Toki/SusuLegend.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int N, a = 0, b = 0; cin >> N;
+    int arr1[N], arr2[N];
+    for (int i = 0; i < N; i++) cin >> arr1[i];
+    for (int i = 0; i < N; i++) cin >> arr2[i];
+    while (a < N && b < N){
+        if (arr1[a] == arr2[b]) b++;    
+        else a++;
+    }
+    if (b == N) cout << "YES";
+    else cout << "NO";
+
+    return 0;
+}
