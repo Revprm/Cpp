8567a1bfc1194864e3fc16f684135b3d68f4ba75
Revprm <Revy.pramana@gmail.com>
2023-01-09 10:03:42 +0700

    Edited

--- C++/Toki/SusuLegend.cpp
+++ C++/Toki/SusuLegend.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
 
 int main(){
+    FAST;
     int N, a = 0, b = 0; cin >> N;
     int arr1[N], arr2[N];
     for (int i = 0; i < N; i++) cin >> arr1[i];
